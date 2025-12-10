b7195109a736cd3c5a78c36cf77b36adda24cc07
Fan744 <135441979+Fan744@users.noreply.github.com>
2025-12-11 00:01:06 +0530

    Update bot configuration values
    Updated configuration values for BOT_TOKEN, APP_ID, API_HASH, and AUTH_USERS.

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ import os
 class Config(object):
 
     # Get a bot token from botfather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8124137115:AAF1mcRDL4jAssXKD-_G_C3far3vwU3Q_ZQ")
 
 
     # Get from my.telegram.org (or @UseTGXBot)
-    APP_ID = int(os.environ.get("APP_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
+    APP_ID = int(os.environ.get("APP_ID", "30157302"))
+    API_HASH = os.environ.get("API_HASH", "0052039fb2fca727868d0228cdaad569")
 
 
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "8255552078").split())
     
